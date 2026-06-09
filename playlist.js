9388746d8d9ea2b42555919d63b3a5c563aab74d
Lane-Web <fajarpro709@gmail.com>
2026-06-09 19:39:48 +0700

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -2,7 +2,7 @@ const playlist = [
   {
     title: "Orion",
     artist: "Kenshi Yonezu-Sangatsu no Lion ",
-    src: "https://drive.google.com/file/d/1hCq2NHHsTdyFCypEFggXO0YMN1twOpSk",
+    src: "https://files.catbox.moe/95z99o.mp3",
     cover: "https://www.lyrical-nonsense.com/wp-content/uploads/2022/04/Kenshi-Yonezu-orion-300x300.jpg"
   }
 ];
